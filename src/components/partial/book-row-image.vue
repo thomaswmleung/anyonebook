70723e4416189c6d38e56e260b710451d4c3fca5
Tommy Lam <tommy.lam@ilearners.hk>
2018-01-11 16:35:25 +0800

    integrate v-viewer in BookRowImage

--- src/components/partial/book-row-image.vue
+++ src/components/partial/book-row-image.vue
@@ -13,19 +13,52 @@
             <v-btn  fab dark small color="primary" class="xsBtn" v-if="show_arrow('up')">
                 <v-icon>keyboard_arrow_up</v-icon>
             </v-btn>
-            <p style="margin-bottom:0px">
-                <img
-                    v-if="getImage()" 
+            <!--
+            <p 
+               v-if="getImage()" 
+               style="margin-bottom:0px">                                 
+                <img                   
                     :src="getImage()"
                     :style="{height:`${row_height-70}px`}"
                     :class="{bw:grey}"   
-                 />
-                <v-card v-if="!getImage()" 
+                 /> 
+            </p> 
+            
+            <div v-if="getImage()">
+                <viewer>
+                    <img 
+                        :src="getImage()" 
+                        :class="{bw:grey}"
+                    />
+                </viewer>
+            </div>--> 
+        <div class="viewer-wrapper" v-if="getImage()">
+          <viewer :options="options" :images="[getImage()]"
+                  @inited="inited"
+                  class="viewer" ref="viewer"
+          >
+            <template slot-scope="scope">
+              <figure class="images">
+                <div class="image-wrapper" >
+                  <img class="image bw"
+                    
+                       :src="getImage()" 
+                       :data-source="getImage()" 
+                       :alt="getImage().split('/').pop()"
+                      :style="{height:`${row_height-70}px`}"
+                  >
+                </div>
+              </figure>
+            </template>
+          </viewer>
+        </div>
+
+
+            <v-card v-if="!getImage()" 
                    :style="{height:`${row_height-70}px`, margin:'auto'}"
                  >
                     <span>{{$t("No Page available")}}</span>
-                </v-card>
-            </p>
+           </v-card>
             <v-btn  fab dark small color="primary" class="xsBtn"  v-if="show_arrow('down')">
                 <v-icon>keyboard_arrow_down</v-icon>
             </v-btn>
@@ -57,6 +90,9 @@ export default {
   methods: {
       ...mapActions([
         ]),
+     inited (viewer) {
+      this.$viewer = viewer
+    },
     getImage(){
         let {
              domain, 
@@ -80,12 +116,12 @@ export default {
         return path;
     },
     show_arrow(direction){
-        let flag=false;
+        let flag=true;
         if(direction=="up"){
-
+            flag=false;
         }
         if(direction=="down"){
-            
+              flag=false;
         }
         if(direction=="left"){
             
@@ -103,6 +139,7 @@ export default {
   },
   data(){
       return{
+          options:{ "inline": true, "button": true, "navbar": false, "title": false, "toolbar": true, "tooltip": true, "movable": true, "zoomable": true, "rotatable": false, "scalable": false, "transition": false, "fullscreen":true, "keyboard": false}
       }
   },
    components:{
@@ -116,6 +153,52 @@ export default {
         margin:0;
     }
     img.bw {
-	filter: grayscale(100);
+	    filter: grayscale(100);
+    }
+
+.viewer-wrapper {
+    position: relative;
+    background: #333;
+    height: 100%;
   }
+  .methods {
+    margin-bottom: 1em;
+    flex-wrap: wrap;
+    & > * {
+      margin-right: 0.75rem;
+    }
+  }
+  .options-panel {
+    .panel-block {
+      padding: 0;
+      .checkbox {
+        display: block;
+        width: 100%;
+        margin: 0;
+        padding: 0.5em 0.75em;
+      }
+    }
+  }
+  .viewer {
+    height: 100%;
+    .images {
+      width:100%;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      padding: 5px;
+      .image-wrapper {
+        display: inline-block;
+        width: calc(33% - 20px);
+        margin: 5px 5px 0 5px;
+        .image {
+          width: 100%;
+          cursor: pointer;
+          display: inline-block;
+        }
+      }
+    }
+  }    
 </style>
